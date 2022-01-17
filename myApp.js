2be396252a2d026464c324454931b4a37b1b6d10
Bartek Florczak <b.florczak@sap.com>
2022-01-17 23:53:17 +0100

    Add nested middleware

--- myApp.js
+++ myApp.js
@@ -1,61 +1,39 @@
-var express = require('express');
-require('dotenv').config();
+var express = require("express");
+require("dotenv").config();
 var app = express();
 
-app.use(function(req, res, next) {
-console.log(`${req.method} ${req.path} - ${req.ip}`);
+app.use(function (req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
     next();
-})
-
-app.use("/public", express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-})
-
-app.get('/json', function(req, res) {
-    var message = 'Hello json';
-    if (process.env.MESSAGE_STYLE === 'uppercase') {
-        message = message.toUpperCase();
-    }
-    res.json({
-        message: message
+  },
+  function (req, res) {
+    res.send({
+        time: req.time
     });
-})
+  }
+);
 
+app.get("/json", function (req, res) {
+  var message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+  res.json({
+    message: message,
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
